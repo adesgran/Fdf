0d215aaf8fb79f99c02e4d64f7c8a4b876bdd6f3
adesgran <adesgran.dev@gmail.com>
2021-12-26 19:53:52 +0100

    update

--- put_line.c
+++ put_line.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 11:45:00 by adesgran          #+#    #+#             */
-/*   Updated: 2021/12/26 13:22:13 by adesgran         ###   ########.fr       */
+/*   Updated: 2021/12/26 19:53:30 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,24 @@ static float	get_origin(t_coord start, float coeff)
 	return (start.y - coeff * start.x);
 }
 
+static unsigned int	get_color(int index, int size, unsigned int color1, unsigned int color2)
+{
+	unsigned int	res;
+	unsigned int	step;
+
+	if (color1 > color2)
+	{
+		step = (color1 - color2) / (unsigned int)size;
+		res = color1 - step * (unsigned int) index;
+	}
+	else
+	{
+		step = (color2 - color1) / (unsigned int)size;
+		res = color1 + step * (unsigned int) index;
+	}
+	return (res);
+}
+
 void	put_line(t_data *img, t_coord start, t_coord end)
 {
 	float	increment;
@@ -31,6 +49,7 @@ void	put_line(t_data *img, t_coord start, t_coord end)
 	float	origin;
 	float	x;
 	float	y;
+	unsigned int	color;
 
 	x = start.x;
 	y = start.y;
@@ -39,14 +58,17 @@ void	put_line(t_data *img, t_coord start, t_coord end)
 		increment = -1;
 	coeff = get_coeff(start, end);
 	origin = get_origin(start, coeff);
+	printf("Line : %d:%d // %d:%d\n", (int)start.x, (int)start.y, (int)end.x, (int)end.y);
 	while (x - end.x >= 0.5 || x - end.x < -0.5)
 	{
-		put_pixel(img, (int)(x), (int)(0.5 + origin + coeff * x), 2805513);
+		color = get_color((int)x, abs((int)start.x - (int)end.x), start.color, end.color);
+		put_pixel(img, (int)(x), (int)(0.5 + origin + coeff * x), color);
 		x += increment;
 	}
 	while (y - end.y >= 0.5 || y - end.y < -0.5)
 	{
-		put_pixel(img, (int)(0.5 + ((y - origin) / coeff)), y, 2805513);
+		color = get_color((int)y, abs((int)start.y - (int)end.y), start.color, end.color);
+		put_pixel(img, (int)(0.5 + ((y - origin) / coeff)), y, color);
 		if (start.y > end.y)
 			y -= 1;
 		else
