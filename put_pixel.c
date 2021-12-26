8fc88df8bf44a85b65f1997a3e66ca79960df37a
adesgran <adesgran.dev@gmail.com>
2021-12-26 17:27:06 +0100

    fix segfault when trying to put_pixel outside of the window

--- put_pixel.c
+++ put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 11:27:11 by adesgran          #+#    #+#             */
-/*   Updated: 2021/12/26 13:22:57 by adesgran         ###   ########.fr       */
+/*   Updated: 2021/12/26 17:19:37 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	put_pixel(t_data *img, int x, int y, ...)
 	char	*dst;
 	
 	va_start(args, y);
-	if (x < 0 || y < 0)
+	if (x < 0 || y < 0 || x >= W_WIDTH || y >= W_HEIGHT)
 		return ;
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = va_arg(args, unsigned int);
