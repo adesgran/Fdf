1b2065ba574bd0f8060d3b9645254a16bfd561ca
adesgran <adesgran.dev@gmail.com>
2021-12-27 20:27:00 +0100

    Need to finish the fix

--- matrix_operations.c
+++ matrix_operations.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 14:24:59 by adesgran          #+#    #+#             */
-/*   Updated: 2021/12/27 19:06:58 by adesgran         ###   ########.fr       */
+/*   Updated: 2021/12/27 20:22:08 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	rotate_x(t_3dcoord *p, float angle)
 	cosa = cos(angle);
 	sina = sin(angle);
 	res.x = p->x;
-	res.y = p->y * cosa - p->z * sina;
-	res.z = p->y * sina + p->z * cosa;
+	res.y = (p->y * cosa) - (p->z * sina);
+	res.z = (p->y * sina) + (p->z * cosa);
 
 	p->x = res.x;
 	p->y = res.y;
@@ -48,9 +48,9 @@ void	rotate_y(t_3dcoord *p, float angle)
 
 	cosa = cos(angle);
 	sina = sin(angle);
-	res.x = p->x * cosa + p->z * sina;
+	res.x = (p->x * cosa) + (p->z * sina);
 	res.y = p->y;
-	res.z = -1 * p->x * sina + p->z * cosa;
+	res.z = (-1 * p->x * sina) + (p->z * cosa);
 
 	p->x = res.x;
 	p->y = res.y;
@@ -65,8 +65,8 @@ void	rotate_z(t_3dcoord *p, float angle)
 
 	cosa = cos(angle);
 	sina = sin(angle);
-	res.x =	p->x * cosa - p->y * sina;
-	res.y = p->x * sina + p->y * cosa;
+	res.x =	(p->x * cosa) - (p->y * sina);
+	res.y = (p->x * sina) + (p->y * cosa);
 	res.z = p->z;
 
 	p->x = res.x;
